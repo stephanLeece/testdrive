7cded535e5a2e26bfa5bfe8378f23828c1a44fec
Stephan Leece <stephan.leece@outlook.com>
2018-01-10 16:07:00 +0100

    initial commit

---
+++ public/script.js
@@ -0,0 +1,12 @@
+// $.ajax({
+//
+//   url: 'https://elegant-croissant.glitch.me/spotify',
+//
+//   data: {
+//
+//
+//   },
+//
+//   success: showResults
+//
+// });
---
+++ server.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const server = express();
+
+server.use(express.static(__dirname + `/public`));
+
+server.get('/', function(req, res) {
+    (res.sendFile('index.html'));
+});
+
+
+
+
+
+server.listen(8080, () => console.log('Listening'));
