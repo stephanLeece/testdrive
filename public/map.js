9c2cca06ceaeef5aa6c69f2cad8852626a77196b
Stephan Leece <stephan.leece@outlook.com>
2018-02-06 19:09:37 +0100

    added map with default attributes

--- public/map.js
+++ public/map.js
@@ -1,1 +1,22 @@
 // a whole buncha googly map code gonna go here
+$(window).on('load', () => {
+
+  function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('gMap'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      };
+if (window.location.pathname == '/info') {
+    initMap();
+    console.log('loading map');
+}
+
+
+
+});
