aa1317dd58bca2d75d212e5456c3246cdb47acd1
Ivana Matijevic <ivanamatijevic@Ivanas-MacBook-Pro-2.local>
2018-02-10 18:14:14 +0100

    test

--- server.js
+++ server.js
@@ -14,39 +14,39 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + `/public`));
 
 app.get('/', function(req, res) {
-  res.render('home',
-  {
-    layout: 'layout'
-  });
+    res.render('home',
+        {
+            layout: 'layout'
+        });
 });
 
 // placeholders for eventresults and catalogueResults returned from database/cms
 
 app.get('/drivedrive', function(req, res) {
-  res.render('drivedrive',
-  {
-    layout: 'layout'
-    // ,events: eventsResults,
-    // catalogue: catalogueResults
-  });
+    res.render('drivedrive',
+        {
+            layout: 'layout'
+            // ,events: eventsResults,
+            // catalogue: catalogueResults
+        });
 });
 
 app.get('/drivedrive/events', function(req, res) {
-  res.render('drivedrive',
-  {
-    layout: 'layout'
-    // ,events: eventsResults,
-    // catalogue: catalogueResults
-  });
+    res.render('drivedrive',
+        {
+            layout: 'layout'
+            // ,events: eventsResults,
+            // catalogue: catalogueResults
+        });
 });
 
 app.get('/drivedrive/catalogue', function(req, res) {
-  res.render('drivedrive',
-  {
-    layout: 'layout'
-    // ,events: eventsResults,
-    // catalogue: catalogueResults
-  });
+    res.render('drivedrive',
+        {
+            layout: 'layout'
+            // ,events: eventsResults,
+            // catalogue: catalogueResults
+        });
 });
 
 // need a get route for testdrive events module -- "testdrive/event/:id"
@@ -54,28 +54,28 @@ app.get('/drivedrive/catalogue', function(req, res) {
 // probably will need the same for drivedrive events modal
 
 app.get('/testdrive', function(req, res) {
-  // need a db.query or req from cms for events information
+    // need a db.query or req from cms for events information
 
-  res.render('testdrive',
-  {
-    layout: 'layout'
-    // ,events: eventsResults
+    res.render('testdrive',
+        {
+            layout: 'layout'
+            // ,events: eventsResults
 
-  });
+        });
 });
 
 app.get('/info', function(req, res) {
-  res.render('info',
-  {
-    layout: 'layout'
-  });
+    res.render('info',
+        {
+            layout: 'layout'
+        });
 });
 
 app.get('/space', function(req, res) {
-  res.render('space',
-  {
-    layout: 'layout'
-  });
+    res.render('space',
+        {
+            layout: 'layout'
+        });
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
