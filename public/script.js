9a840a533987d6a63603f712c2cd6954e2ea0f31
Stephan Leece <stephan.leece@outlook.com>
2018-02-25 14:51:59 +0100

    logic for testdrive, menu for info

--- public/script.js
+++ public/script.js
@@ -1,18 +1,31 @@
-function showModal() {
-    console.log("running modal");
+// ---------------------------------- homepage functions -----------------
 
-    $("#home-render-top").on('click', function(){
-        console.log("hi h1");
-        $('#modal').css({
-            "visibility":"visible"
-        })
-    })
+function showModal() {
+  console.log("running modal");
+
+  $("#home-render-top").on('click', function() {
+    console.log("hi h1");
+    $('#modal').css({"visibility": "visible"})
+  })
 }
 showModal();
 
 function fadeIntoImg() {
-    console.log("lag?");
-$('#intro').fadeOut(5000);
+  console.log("lag?");
+  $('#intro').fadeOut(5000);
 
 }
 // fadeIntoImg();
+
+// ------------------------------- testdrive functions ------------------
+
+// swap out for ajax request to contentful or server route to populate with event deets
+events = ['main1, main2, main3, main4']
+
+$('li').on('click', e => {
+  $('#eventFull')
+    .empty()
+    .append(`<p> ${$(e.target).attr('class')} </p>`)
+
+
+});
