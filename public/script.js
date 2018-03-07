712b4f953e961e239db9e54b5af6f022b717f373
Ivana Matijevic <ivanamatijevic@Slavic-Thunder.fritz.box>
2018-03-07 22:39:24 +0100

    attempting to flex homepage

--- public/script.js
+++ public/script.js
@@ -1,51 +1,51 @@
 // ---------------------------------- homepage functions -----------------
 
-let backButton = $("#back-button")
+let backButton = $("#back-button");
 
 backButton.on('click', () => {
-  window.history.back();
-})
+    window.history.back();
+});
 
 function showModal() {
-  console.log("running modal");
+    console.log("running modal");
 
-  $("#gallery-space-img").on('click', function() {
-    console.log("hi h1");
-    $('#modal').css({"visibility": "visible"})
-  })
+    $("#container").on('click', function() {
+        console.log("hi h1");
+        $('#modal').css({"visibility": "visible"});
+    });
 }
 showModal();
 
 function fadeIntoImg() {
-  console.log("lag?");
-  $('#halk').fadeOut(3000);
+    console.log("lag?");
+    $('#halk').fadeOut(3000);
 }
 // function fadeIntoImg(body) {
 //   console.log("lag?");
 //   $('body').fadeOut(3000);
 // }
 //
-fadeIntoImg();
+// fadeIntoImg();
 
 // ------------------------------- testdrive/events functions ------------------
 
 // $(parentSelector).find(childSelector)
 $('.eventsInfoBox').find('div').addClass('hidden');
 $('li').on('click', e => {
-  let eventToShow = '.' + $(e.target).attr('class')
-$('.eventsInfoBox').find('div').addClass('hidden');
-$('.eventsInfoBox').find(`${eventToShow}`).removeClass('hidden');
+    let eventToShow = '.' + $(e.target).attr('class');
+    $('.eventsInfoBox').find('div').addClass('hidden');
+    $('.eventsInfoBox').find(`${eventToShow}`).removeClass('hidden');
 
 });
 
 // ------------------------------- video functions ----------
 
-let video = $('.video')
+let video = $('.video');
 
 function play() {
-  video.play()
+    video.play();
 }
 
 function pause() {
-  video.pause()
+    video.pause();
 }
