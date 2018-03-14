ff5a245302003e7319089eed7984a32d1c17a5c2
elenasavvidou <elektroniki@Macintosh.lan>
2018-03-14 16:15:16 +0100

    color orange active depending on route + some scss

--- public/styles.js
+++ public/styles.js
@@ -5,41 +5,51 @@ if (window.location.pathname == '/info') {
     });
 
 }
+
+
+$(function(){
+    var current = location.pathname;
+    $('#cat').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
+
+$(function(){
+    var current = location.pathname;
+    $('#eve').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('act');
+        }
+    })
+})
+
+
+// ('.events-menu').on('p', 'img' function(){
+//    $(this).addClass('visible').siblings().removeClass('visible');
+// });
+
+
+// trying the rainbow scroll
+
+// var $head = $('head');
+// var $body = $('body');
 //
-// if (window.location.pathname == '/catalogue') {
-//   $('.cat').css({
-//       color:orange;
-//     });
+// $head.append("<style class='rainbow-scroll'>body::-webkit-scrollbar-thumb{background-color: #edb91d;}</style>");
+// var $rainbowScroll = $('.rainbow-scroll');
 //
+// $(window).scroll(function(){
+//   $rainbowScroll.html(".rainbow-scroll::-webkit-scrollbar-thumb{background-color:"+ getRandomColor() +"}")
+//   $body.addClass('rainbow-scroll');
+//   $body.css({overflow: 'auto'});
+//   setTimeout( function(){ $body.css({overflow: 'scroll'}) }, 0);
+// });
+//
+// function getRandomColor(){
+//   return '#'+Math.floor(Math.random()*16777215).toString(16);
 // }
-//
-// $(function(){
-//     // this will get the full URL at the address bar
-//     var url = window.location.href;
-//
-//     // passes on every "a" tag
-//     $("#sub-header a").each(function() {
-//             // checks if its the same on the address bar
-//         if(url == (this.href)) {
-//             $(this).closest("li").addClass("active");
-//         }
-//     });
-// });
-
-
-// $(function() {
-//   $('ul a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-// });
-
-// 
-// function getPageName(url) {
-//     var index = url.lastIndexOf("/") + 1;
-//     var filenameWithExtension = url.substr(index);
-//     var filename = filenameWithExtension.split(".")[0]; // <-- added this line
-//     return filename;                                    // <-- added this line
-// }
-//
-// $(function(){
-//    var currentPageName = getPageName(window.location.pathname);
-//    $('#' + currentPageName).addClass('active');
-// });
