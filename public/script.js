9b4338f8a3829b7d79c714550098f230936bd57d
Stephan Leece <stephan.leece@outlook.com>
2018-03-22 18:48:22 +0100

    beginning up/down logic

--- public/script.js
+++ public/script.js
@@ -37,13 +37,17 @@ function showModal() {
 
 $(function() {
     //Enable swiping...
-    $(window).swipe( {
+    $('#gallery-space-img').swipe( {
       //Generic swipe handler for all directions
       swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-        console.log("You swiped " + direction );
+        if (direction == "up") {
+          console.log('up!');
+        } else if (direction == "down") {
+          console.log('down!');
+        }
       },
       //Default is 75px, set to 0 for demo so any distance triggers swipe
-       threshold:0
+       threshold:50
     });
   });
 
