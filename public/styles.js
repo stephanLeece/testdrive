7209698b524029854100bd3d585f2e05534d4686
elenasavvidou <savvidou.eleni@gmail.com>
2018-04-27 23:42:00 -0700

    my last try to get the scroll and active working

--- public/styles.js
+++ public/styles.js
@@ -1,17 +1,16 @@
 if ($(window).width() > 600) {
-    $(".mob-container").hide();
-    showDesktopEvents();
+  $(".mob-container").hide();
+  showDesktopEvents();
 } else {
-    $(".mob-container").show();
-    showMobileEvents();
+  $(".mob-container").show();
+  showMobileEvents();
 }
 
 if ($(window).width() < 600 && window.location.pathname == '/testdrive') {
   $(".mob-container").show();
   $("#makeitblack").hide();
   $("#backButton").hide();
-}
-else if (window.location.pathname == '/testdrive'){
+} else if (window.location.pathname == '/testdrive') {
   $(".drivedrive-menu").hide();
   $("#makeitblack").hide();
 }
@@ -27,6 +26,8 @@ function showDesktopEvents() {
 
 
 
+///// MOBILE EVENTS PAGE ///// ///// MOBILE EVENTS ///// ///// MOBILE EVENTS ///// ///// MOBILE EVENTS ///// ///// MOBILE EVENTS /////
+
 
 function showMobileEvents() {
   $('.eventsInfoBox').find('div').addClass('hidden');
@@ -37,42 +38,43 @@ function showMobileEvents() {
   });
   // hides the infobox and all of the divs within it
   // $('.eventsInfoBox').find('div').addClass('hidden');
-  $('.eventsInfoBox').css({ 'display': 'none' });
+  $('.eventsInfoBox').css({'display': 'none'});
   let highlightedEvent = $('.eventList').children('.ddEventActive');
+
   $(".eventDetails").click(function(e) {
     if (highlightedEvent !== e.target) {
       highlightedEvent.removeClass('.ddEventActive');
     }
     // trying to put the background image behind the selected event
-    // $(e.target).addClass('ddEventActive');
-    // let eventIndex = $(this).index();
+    $(e.target).addClass('ddEventActive');
+
+
+
+    let eventIndex = $(this).index();
     // $('.eventList').children().css({ 'display': 'none' });
-    $('.eventList').children('.ddEventActive').css({ 'display': 'block' });
-    $('.eventsInfoBox').css({ 'display': 'flex' });
+    $('.eventList').children('.ddEventActive').css({'display': 'block'});
+    $('.eventsInfoBox').css({'display': 'flex'});
     // $('.eventsInfoBox').find('div').addClass('hidden');
     $(`.eventInfo:nth-child(${eventIndex + 1})`).removeClass('hidden');
     // w/o this the infobox will never appear
     // e.stopPropagation();
   });
-  // $('#eve-disp').click(function() {
-  //   // returns the infobox to hidden -- the 'exit' functionality
+
+  $('.eventDetails').click(function() {
+  //    returns the infobox to hidden -- the 'exit' functionality
   //   $('.eventsInfoBox').css({ 'display': 'none' });
-  //   $('.eventList').children('.ddEventActive').removeClass('.ddEventActive')
-  // })
+    $('.eventDetails').children('.ddEventActive').removeClass('.ddEventActive')
+  })
 
   $('.trigger, .slider').click(function() {
-    $('.eventsInfoBox').toggleClass('close');
+    $('.slider, .eventsInfoBox').toggleClass('close');
   });
 
 }
 
-
 // -------- view dependent styling --------------
 if (window.location.pathname == '/info') {
-    $('body').css({
-        'background': 'url(./assets/Elements/Info/Info-bgA.png) no-repeat center center fixed',
-        'background-size': '100vw 100vh'
-    });
+  $('body').css({'background': 'url(./assets/Elements/Info/Info-bgA.png) no-repeat center center fixed', 'background-size': '100vw 100vh'});
 }
 
 
